35c53469658f9445eb991b9624c30438392d53d5
Mike Hingley <Computa_Mike@hotmail.com>
2016-12-03 22:26:20 +0000

    Adding Basic Unity first draft
    First basic draft of Unity Game

---
+++ ColinsCrocs/Assets/CameraMovement.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMovement : MonoBehaviour {
+    public Transform LookAt;
+    public float XOffset;
+    public float YOffset;
+    public float ZOffset;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        transform.position = LookAt.transform.position + new Vector3(XOffset, YOffset, ZOffset);
+
+
+    }
+}
